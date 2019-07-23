6a1ba0413c23e1fb22e42bc3967179a241b29c40
ANDRES-ANTE\asus <aante@espol.edu.ec>
2019-07-23 01:15:22 -0500

    Se agrego la clave Api de la apl de la pagina web www.console.developers.google.com

--- app/src/main/java/com/example/amst7/MainActivity.java
+++ app/src/main/java/com/example/amst7/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.amst7;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnMap;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMap = (Button) findViewById(R.id.btn_Map);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MapActivity1.class);
+                startActivity(intent);
+            }
+        });
     }
-    public void openMap(View v){
-    
-    }
+
 }
+
